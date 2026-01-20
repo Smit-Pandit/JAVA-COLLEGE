bce5f06bd1012c4325bab7c42f8400b2f983e1e5
Smit <smitpandit200713@gmail.com>
2026-01-20 14:42:27 +0530

    initial commit

---
+++ com.lab3/figure.java
@@ -0,0 +1,16 @@
+// Q1.)Design a class called figure . It must have three attributes r,aand v of the data type douoble.It must have the
+// following methods:
+// public void dispArea(); public void dispVolume();
+// The method dispArea must have a statement to print the volume of a. The method dispVolume();
+// must have a statement to print the value of v.Design a class cone . It must extends class figure class .It must also
+// have the following attributes h,s of type double.It must have following methods:
+// public void calcArea();public void calcVolume();
+// The formulae for the area and volume of the cone are as follows:
+// Area of a cone(p*r*s)+(p*r*s)
+// volume of a con=(p*r*s)/3
+// Write a main method in a class called inheritance demo to demonstrate inheritance.
+public class figure{
+    public static void main(String[] args) {
+        
+    }
+}
