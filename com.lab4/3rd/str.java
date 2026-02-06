fbcfb4acf0f9f77753a23a4c9a85f76de4884ee2
Smit <smitpandit200713@gmail.com>
2026-02-07 00:58:34 +0530

    completed 3rd que lab 4

--- com.lab4/3rd/str.java
+++ com.lab4/3rd/str.java
@@ -1,16 +1,37 @@
-// We need to write the function to check the password entered is correct or not based on the following
-// conditions.
-// a) It must have atleast one lower case character and one digit.
-// b) It must not have any Upper case characters and any special characters
-// c) length should be b/w 5-12.
-// d) It should not have any same immediate patterns like
-// abcanan1 : not acceptable coz of an an pattern
-// abc11se: not acceptable, coz of pattern 11
-// 123sd123 : acceptable, as not immediate pattern
-// adfasdsdf : not acceptable, as no digits
-// Aasdfasd12: not acceptable, as have uppercase character
+
+import java.util.Scanner;
+// A string is entered like aabcdeaabcjlkjerwlaaabbsadfdsf...
+// And you have to print those characters which exceed a given no. n entered by user.
+
 public class str {
+
     public static void main(String[] args) {
-        
+        try {
+            Scanner s = new Scanner(System.in);
+            System.out.print("Enter n : ");
+            int n = s.nextInt();
+            int x;
+            s.nextLine();
+            System.out.print("Enter the string : ");
+            String str = s.nextLine();
+            char[] a = str.toCharArray();
+            for (int i = 0; i < str.length(); i++) {
+                x = 0;
+                for (int j = 0; j < str.length(); j++) {
+                    if (a[i] == a[j]) {
+                        x++;
+                    }
+                }
+                if (x >= n) {
+                    System.out.println(a[i] + " exceeds the limit");
+                    return;
+                }
+            }
+            System.out.println("All Good");
+            s.close();
+        }
+        catch (Exception e) {
+            System.out.println("Error " + e);
+        }
     }
 }
