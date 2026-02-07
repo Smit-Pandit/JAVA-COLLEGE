acc2a8b212dff30ed9bd85f74217d31209e3619c
Smit <smitpandit200713@gmail.com>
2026-02-07 12:20:22 +0530

    initial commit

---
+++ com.lab5/7th/quiz.java
@@ -0,0 +1,7 @@
+//Assume that the pass marks are 5 out of 8.the program stores the correct answers in an array.the
+//submitted answers are specifies as program arguments. Let X represents a question that was not
+//answered on the quiz.Use an enum type to represent the result of answering a question.
+//The program calculates and prints a report along the following lines:
+public class quiz {
+    
+}
