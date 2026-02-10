073c6c07c3260eb80760427e607cb9078bbd1e2a
Smit <smitpandit200713@gmail.com>
2026-02-10 15:40:11 +0530

    random

--- string_function/Std.java
+++ string_function/Std.java
@@ -1,15 +1,18 @@
+import java.util.Arrays;
+
 public class Std {
-    int arrn[];
+    // int arrn[];
     public int length(char arr[]){
         return arr.length;
     }
     public char[] add(char arr[],int at,char value){
-        int l = arr.length;
         int i;
-        for(i = l;i>at;i--){
-            arr[i]=arr[i-1];
+        char []anew = Arrays.copyOf(arr, length(arr)+1);
+        int l=arr.length;
+        for(i=l;i>at;i--){
+            anew[i] = arr[i-1];
         }
-        arr[i]=value;
-        return arr;
+        anew[i] = value;
+        return anew;
     }
 }
