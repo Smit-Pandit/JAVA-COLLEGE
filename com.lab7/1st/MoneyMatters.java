d10f255bef6e985b190281e62513090ecbca2f3c
Smit <smitpandit200713@gmail.com>
2026-02-17 15:30:21 +0530

    initial commit

---
+++ com.lab7/1st/MoneyMatters.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+// Develop  a  program  that  accepts  an  initial  amount  of  money  (called  the  principal),  a  simple  annual  interest 
+// rate,  and  a  number  of  months  will  compute  the  balance  at  the  end  of  that  time.  Assume  that  no  additional 
+// deposits  or  withdrawals  are  made  and  that  a  month  is  1/12  of  a  year.  Total  interest  is  the  product  of  the 
+// principal, the annual interest rate expressed as a decimal, and the number of years.
+public class MoneyMatters{
+    public static void main(String[] args) {
+        try(Scanner s = new Scanner(System.in)){
+            
+        }
+    }
+}
