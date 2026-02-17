508e3f8c6d4911eb0df553f0075f8b2e4c9e3da3
Smit <smitpandit200713@gmail.com>
2026-02-17 14:44:56 +0530

    try catch block

--- com.lab5/6th/exam.java
+++ com.lab5/6th/exam.java
@@ -5,44 +5,54 @@
 import java.util.Scanner;
 
 public class exam {
+
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter student name : ");
-        String name = s.nextLine();
-        System.out.print("Enter the marks in 1st subject : ");
-        int mark1 = s.nextInt();
-        System.out.print("Enter the marks in 2nd subject : ");
-        int mark2 = s.nextInt();
-        System.out.print("Enter the marks in 3rd subject : ");
-        int mark3 = s.nextInt();
-        student st = new student(name, mark1, mark2, mark3);
-        System.out.println("Student Name: " + st.getName());
-        st.displayResult();
-        s.close();
+        try (Scanner s = new Scanner(System.in);) {
+            System.out.print("Enter student name : ");
+            String name = s.nextLine();
+            System.out.print("Enter the marks in 1st subject : ");
+            int mark1 = s.nextInt();
+            System.out.print("Enter the marks in 2nd subject : ");
+            int mark2 = s.nextInt();
+            System.out.print("Enter the marks in 3rd subject : ");
+            int mark3 = s.nextInt();
+            student st = new student(name, mark1, mark2, mark3);
+            System.out.println("Student Name: " + st.getName());
+            st.displayResult();
+            s.close();
+        }catch(NumberFormatException e){
+            System.out.println("Enter a valid number");
+        }
     }
-    
+
 }
-class student{
-    int m1,m2,m3;
+
+class student {
+
+    int m1, m2, m3;
     String name;
-    student(String name ,int m1, int m2, int m3) {
+
+    student(String name, int m1, int m2, int m3) {
         this.name = name;
         this.m1 = m1;
         this.m2 = m2;
         this.m3 = m3;
     }
-    float calcAVG(){
-        float avg = (m1+m2+m3)/3;
+
+    float calcAVG() {
+        float avg = (m1 + m2 + m3) / 3;
         return avg;
     }
+
     String getName() {
         return name;
     }
+
     void displayResult() {
         if (calcAVG() >= 50) {
-            System.out.println("The student "+ name + " has PASSED the exam.");
+            System.out.println("The student " + name + " has PASSED the exam.");
         } else {
-            System.out.println("The student"+ name +"has FAILED the exam.");
+            System.out.println("The student" + name + "has FAILED the exam.");
         }
     }
 }
