1752c38c9eefcf44c43552f9ccb6b09b5f631a07
Smit <smitpandit200713@gmail.com>
2026-02-17 14:47:47 +0530

    initial commit

---
+++ com.lab5/2nd/hexadecimal.java
@@ -0,0 +1,7 @@
+// write a complete java program to test whether a given number is a hexadecimal number or not .if it is not ,an 
+// exception  should  be  throwns  and  processed  by  the  program  .the  exception  must  be  userdefined    .the  numbere  
+// must  be  assigned  to  string  variable.String  function  must  be  used  to  processing.irrespective  of  whether  the 
+// number is a hexadecimal number or not the string Endingthe program must be printed on the screen.
+public class hexadecimal {
+    
+}
