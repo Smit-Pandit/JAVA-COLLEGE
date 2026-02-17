b780beeda37562775918e6b133cfd78c3302f3cc
Smit <smitpandit200713@gmail.com>
2026-02-17 14:41:55 +0530

    try catch block

--- com.lab5/5th/tax.java
+++ com.lab5/5th/tax.java
@@ -1,33 +1,43 @@
+
 import java.util.Scanner;
 // Utopias tax accountants always use programs that compute income taxes even though the tax rate is a solid,
 // never- changing 15%.Define the program calculate Tax which determines the tax on the gross pay. Define
 // calculate NetPay that determines the net pay of an employee from the number of hours worked. Assume an
 // hourly rate of $12.
+
 public class tax {
 
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.print("Working hours : ");
-        int H = s.nextInt();
-        UtopiasTax pay = new UtopiasTax(H);
-        System.out.println("The Net Payment is : "+ pay.netPay()+"$");
-        s.close();
+        try (Scanner s = new Scanner(System.in);) {
+            System.out.print("Working hours : ");
+            int H = s.nextInt();
+            UtopiasTax pay = new UtopiasTax(H);
+            System.out.println("The Net Payment is : " + pay.netPay() + "$");
+            s.close();
+        }catch(NumberFormatException e){
+            System.out.println("Enter a valid Number");
+        }
     }
 }
 
 class UtopiasTax {
+
     int hours;
     final double HourlyRate = 12;
+
     UtopiasTax(int H) {
         this.hours = H;
     }
+
     double netPay() {
         return calculateGrossPay() - calculateTax();
     }
+
     double calculateTax() {
         return calculateGrossPay() * 0.15;
     }
+
     double calculateGrossPay() {
-        return  HourlyRate * hours;
+        return HourlyRate * hours;
     }
 }
