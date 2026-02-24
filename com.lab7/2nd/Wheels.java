b295634c36cba8efb158d3236d243dd7fd7fdf0d
Smit <smitpandit200713@gmail.com>
2026-02-24 14:58:46 +0530

    wheels over

--- com.lab7/2nd/Wheels.java
+++ com.lab7/2nd/Wheels.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 // Write a class vehicle .Define suitable attributes and methods. 
 // Write subclasses of Vehicle like Car, Bicycle,  Scooter.  
 // Assume suitable required attributes. 
@@ -5,14 +8,32 @@
 // Which prints the maximum speed of the vehicle? 
 // (Use of super keyword is expected in the constructor of  inherited classes). 
 public class Wheels{
-    public static void main(String[] args) {
-        car myCar = new car("Sedan");
-        myCar.maxSpeed();
+    public static void main(String[] args) throws Exception {
+        try(Scanner s = new Scanner(System.in);){
+            System.out.println("Enter the type of car");
+            String type = s.nextLine().toLowerCase();
+            car c = new car(type);
+            c.start();
+            c.stop();
+            c.maxSpeed();
+
+            System.out.println("Enter the cc of bike");
+            int cc = s.nextInt();
+            bike b = new bike(cc);
+            b.start();
+            b.stop();
+            b.maxSpeed();
+
+            System.out.println("For scotter");
+            scotter st = new scotter();
+            st.start();
+            s.close();
+        }
     }
 }
 class vehicle{
     public void maxSpeed(){
-        System.out.println("Maximum speed of  vehicle is 0 km/h");
+        System.out.println("Maximum speed of vehicle is 0 km/h");
     }
     public void start(){
         System.out.println("Starting the vehicle...");
@@ -26,19 +47,43 @@ class car extends vehicle{
     String type;
     int speed = 0;
 
-    public car(String type) {
+    car(String type) throws Exception {
         this.type = type;
         switch (type) {
             case "sedan" -> speed = 140;
             case "xuv" -> speed = 200;
             case "super" -> speed = 300;
             case "hyper" -> speed = 500;
-            default -> speed = 0;
+            default -> throw new Exception("Not a valid type of car");
         }
     }
-
     @Override
     public void maxSpeed() {
         System.out.println("Maximum speed of " + type + " : "+ speed + "km/h");
+    }
+}
+class bike extends vehicle{
+    int cc,speed=0;
+    bike(int cc) throws Exception{
+        this.cc = cc;
+        switch(cc){
+            case 50 -> speed = 70;
+            case 125 -> speed = 100;
+            case 250 -> speed = 150;
+            case 300 -> speed = 200;
+            case 700 -> speed = 280;
+            case 1000 -> speed = 300;
+            case 1100 -> speed = 306;
+            default -> throw new Exception("Not a valid cc for bike");
+        }
+    }
+    @Override
+    public void maxSpeed() {
+        System.out.println("Maximum speed of bike with" + cc + "cc is "+ speed + "km/h");
+    }
+}
+class scotter extends vehicle{
+    scotter() throws Exception{
+        throw new Exception("Wrong vehicle");
     }
 }
