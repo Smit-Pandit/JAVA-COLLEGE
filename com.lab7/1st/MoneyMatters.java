01bf85ec5e5cae9b620a8b280ac4de2406eb080b
Smit <smitpandit200713@gmail.com>
2026-02-24 15:14:26 +0530

    q1 vhs architecture

--- com.lab7/1st/MoneyMatters.java
+++ com.lab7/1st/MoneyMatters.java
@@ -19,9 +19,3 @@ public class MoneyMatters{
         }
     }
 }
-class bank{
-    float calcInterest(int p,float r,int t) {
-        float SI = p*r*t/100;
-        return SI;
-    }
-}
---
+++ com.lab7/1st/bank.java
@@ -0,0 +1,6 @@
+class bank{
+    float calcInterest(int p,float r,int t) {
+        float SI = p*r*t/100;
+        return SI;
+    }
+}
